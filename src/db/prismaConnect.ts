d09b09b7e19f7c92ecc1c804350c4b5948807714
iyanu <tobiy23@gmail.com>
2026-01-30 05:30:36 +0100

    update

--- src/db/prismaConnect.ts
+++ src/db/prismaConnect.ts
@@ -1,6 +1,7 @@
-import { PrismaClient } from "../generated/prisma/client.ts";
+import { PrismaClient } from "@prisma/client.ts";
 import { withAccelerate } from '@prisma/extension-accelerate'
 import "dotenv/config";
+
 
 
 const prisma = new PrismaClient({
